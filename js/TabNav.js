2cedb9f4e47cc98ac051120fa39eb2e97b2b2c3f
Natthan <natthandosantos@gmail.com>
2022-07-04 20:49:33 -0300

    add basic js to navigate tabs

---
+++ js/TabNav.js
@@ -0,0 +1,8 @@
+function changeTab(tabName) {
+    var i;
+    var x = document.getElementsByClassName("tab-content");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    document.getElementById(tabName).style.display = "block";
+  }
